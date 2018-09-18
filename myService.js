532a0a2d179edb525871e24277add739eb32c43d
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-09-17 18:22:52 -0600

    first commit

---
+++ myService.js
@@ -0,0 +1,15 @@
+var http = require('http'); var express = require('express');
+var app = express();
+var GPIOCtrl = require('./GPIOcontroller.js');
+app.get('/unlock/', function(req, res){ 
+	GPIOCtrl.unlockRelay();
+ });
+
+//  NSURL *restURL = [NSURL URLWithString:@"http://raspberrypi-address:port/unlock/"];
+// NSURLRequest *restRequest = [NSURLRequest requestWithURL:restURL];
+// currentConnection = [[NSURLConnection alloc] initWithRequest:restRequest delegate:self];
+
+
+app.listen(3000); 
+console.log('App Server running at port 3000');
+
